ebe9436783efa2edf59adeff0f8a0d90a8c2f9f2
Jörn Hirschfeld <joern.hirschfeld@vier.ai>
2022-03-02 09:49:27 +0100

    Set package name to cvg_sdk

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ CLIENTS=("assist.yml" "dialog.yml" "call.yml")
 for f in "${CLIENTS[@]}"
 do
   FILE=api-specs/$f
-	docker-entrypoint.sh generate -g python -p projectName=cvg-python-sdk -o $PWD/out -i $FILE
+  docker-entrypoint.sh generate -g python -p projectName=cvg-python-sdk -p packageName=cvg_sdk -o $PWD/out -i $FILE
 done 
