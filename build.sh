60a30998ea21b42fc926d8bdbe6b1dfcf1ee6762
Jörn Hirschfeld <joern.hirschfeld@vier.ai>
2022-07-29 14:49:44 +0200

    added version

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 # Merge apis/__init__.py
 CLIENTS=("assist.yml" "dialog.yml" "call.yml")
+VERSION="1.0.1"
 
 for f in "${CLIENTS[@]}"
 do
   FILE=api-specs/$f
-  docker-entrypoint.sh generate -g python -p projectName=cvg-python-sdk -p packageName=cvg_sdk -o $PWD/out -i $FILE
+  docker-entrypoint.sh generate -g python -p packageVersion=$VERSION -p projectName=cvg-python-sdk -p packageName=cvg_sdk -o $PWD/out -i $FILE
 done 
