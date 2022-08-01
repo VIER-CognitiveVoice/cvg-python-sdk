90b8f3b50569cb88db32e7391e5810ec50cb6eb6
Phillip Schichtel <phillip.schichtel@vier.ai>
2022-08-01 11:29:50 +0200

    redo the build logic

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 # Merge apis/__init__.py
-CLIENTS=("assist.yml" "dialog.yml" "call.yml")
-VERSION="1.0.1"
+clients=("assist" "dialog" "call" "recording")
+version="0.1.0"
+openapi_generator_image="openapitools/openapi-generator-cli:latest"
+input_base="${INPUT_BASE:-"https://cognitivevoice.io/specs/specs"}"
 
-for f in "${CLIENTS[@]}"
+container_build_dir="/build"
+output_folder="out"
+package_name="cvg_sdk"
+mkdir "$output_folder"
+
+for client in "${clients[@]}"
 do
-  FILE=api-specs/$f
-  docker-entrypoint.sh generate -g python -p packageVersion=$VERSION -p projectName=cvg-python-sdk -p packageName=cvg_sdk -o $PWD/out -i $FILE
-done 
+    docker run --rm -t -u "$(id -u):$(id -g)" -v "${PWD}:${container_build_dir}" "$openapi_generator_image" generate -g python -p packageVersion=$version -p projectName=cvg-python-sdk -p packageName="$package_name" -o "${container_build_dir}/${output_folder}" -i "${input_base}/${client}.yml"
+done
+
+rm "${output_folder}/${package_name}/__init__.py"
+VERSION="$version" envsubst '$VERSION' < setup.py.in > "${output_folder}/setup.py"
+
+cd "$output_folder"
+python setup.py sdist
+python setup.py bdist_wheel
+
