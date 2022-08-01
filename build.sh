07cb7f99c9077409f474ecef69c2b915004adfd5
Phillip Schichtel <phillip.schichtel@vier.ai>
2022-08-01 12:41:30 +0200

    keep an empty __init__.py

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ do
     docker run --rm -t -u "$(id -u):$(id -g)" -v "${PWD}:${container_build_dir}" "$openapi_generator_image" generate -g python -p packageVersion=$version -p projectName=cvg-python-sdk -p packageName="$package_name" -o "${container_build_dir}/${output_folder}" -i "${input_base}/${client}.yml"
 done
 
-rm "${output_folder}/${package_name}/__init__.py"
+echo "" > "${output_folder}/${package_name}/__init__.py"
 VERSION="$version" envsubst '$VERSION' < setup.py.in > "${output_folder}/setup.py"
 
 cd "$output_folder"
