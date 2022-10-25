ff9f1d564baf0e3d037c826f5f8be4487f44aafc
Phillip Schichtel <phillip.schichtel@vier.ai>
2022-10-25 14:56:52 +0200

    align the build process with the channel

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # Merge apis/__init__.py
 clients=("assist" "dialog" "call" "recording" "health")
@@ -10,17 +12,22 @@ input_base="${INPUT_BASE:-"https://cognitivevoice.io/specs/specs"}"
 container_build_dir="/build"
 output_folder="out"
 package_name="cvg_sdk"
+if [ -e "$output_folder" ]
+then
+  rm -R "$output_folder"
+fi
 mkdir "$output_folder"
 
 for client in "${clients[@]}"
 do
-    docker run --rm -t -u "$(id -u):$(id -g)" -v "${PWD}:${container_build_dir}" "$openapi_generator_image" generate -g python -p packageVersion=$version -p projectName=cvg-python-sdk -p packageName="$package_name" -o "${container_build_dir}/${output_folder}" -i "${input_base}/${client}.yml"
+    docker run --rm -t -u "$(id -u):$(id -g)" -v "${PWD}:${container_build_dir}" "$openapi_generator_image" generate -g python -p packageVersion="$version" -p projectName=cvg-python-sdk -p packageName="$package_name" -o "${container_build_dir}/${output_folder}" -i "${input_base}/${client}.yml"
 done
 
 echo "" > "${output_folder}/${package_name}/__init__.py"
-VERSION="$version" envsubst '$VERSION' < setup.py.in > "${output_folder}/setup.py"
+cp requirements.txt out
 
 cd "$output_folder"
-python setup.py sdist
-python setup.py bdist_wheel
+export VERSION="$version"
+python ../setup.py sdist
+python ../setup.py bdist_wheel
 
--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 from setuptools import setup, find_packages  # noqa: H301
+import os
 
-NAME = "cvg-python-sdk"
-VERSION = "${VERSION}"
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
+if "VERSION" in os.environ and len(os.environ["VERSION"]) > 0:
+    version = os.environ["VERSION"]
+else:
+    version = "0.0.1-devel"
 
-REQUIRES = [
-  "urllib3 >= 1.25.3",
-  "python-dateutil",
-]
+with open("requirements.txt") as f:
+    requirements = [req.strip() for req in f.readlines() if len(req) > 0]
+
+print(requirements)
+exit(1)
 
 setup(
-    name=NAME,
-    version=VERSION,
+    name="cvg-python-sdk",
+    version=version,
     description="VIER Cognitive Voice Gateway SDK",
     author="VIER GmbH",
     author_email="support@vier.ai",
     url="https://cognitivevoice.io",
     keywords=["OpenAPI", "OpenAPI-Generator", "VIER", "VIER Cognitive Voice Gateway SDK"],
     python_requires=">=3.6",
-    install_requires=REQUIRES,
+    install_requires=requirements,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
     license="MIT",
